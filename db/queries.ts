b761f3f7148612a735a2c959a6905c2c1c77a1a2
Senheng App <senhengapp@ZZ---Senhengs-MacBook-Pro.local>
2024-05-26 00:11:54 +0800

    Updated the project with unit and lesson button

--- db/queries.ts
+++ db/queries.ts
@@ -3,6 +3,7 @@ import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 import {
+  challengeProgress,
   courses,
   units,
   userProgress } from "@/db/schema";
@@ -25,12 +26,14 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
+  const { userId } = await auth();
   const userProgress = await getUserProgress();
 
-  if (!userProgress?.activeCourseId) {
+  if (!userId || !userProgress?.activeCourseId) {
     return [];
   }
 
+  // TODO: Confirm whether order is needed
   const data = await db.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
@@ -38,7 +41,11 @@ export const getUnits = cache(async () => {
         with: {
           challenges: {
             with: {
-              challengeProgress: true,
+              challengeProgress: {
+                where: eq(
+                  challengeProgress.userId, userId,
+                ),
+              },
             },
           },
         },
@@ -61,6 +68,7 @@ export const getUnits = cache(async () => {
     return { ...unit, lessons: lessonsWithCompletedStatus };
   });
 
+  return normalizedData;
 });
 
 export const getCourses = cache(async () => {
